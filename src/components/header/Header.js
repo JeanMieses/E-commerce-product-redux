5971cba5a181705525c7408113f46bd3d738f692
JeanMieses <araujo0275@gmail.com>
2022-01-21 16:28:17 -0500

    adding navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import classes from './Header.module.css';
+import iconCart from './../../assets/icon-cart.svg';
+import iconMenu from './../../assets/icon-menu.svg';
+import imageavatar from './../../assets/image-avatar.png'
+
 const Header = () => {
-    return <header>
+    const [showOptions, setShowOptions] = useState(false);
+
+    const showOptionsHandler = () => {
+        setShowOptions(true);
+    }
+
+    const hideOptionsHandler = () => {
+        setShowOptions(false)
+    }
+
+    return <header className={classes.header}> 
         <nav>
             <div>
-                <button>click me</button>
+                <button className={classes.menu} onClick={showOptionsHandler}> <img src={iconMenu} /> </button>
                 <h1>snealers</h1>
 
-                <ul>
+                <ul className={showOptions? '' : classes.hide}>
+                    <button onClick={hideOptionsHandler}>X</button>
                     <li>collection</li>
                     <li>men</li>
                     <li>women</li>
@@ -15,8 +32,8 @@ const Header = () => {
             </div>
 
             <div>
-                <img/>
-                <img/>
+                <img className={classes.cartimg} src={iconCart}/>
+                <img  className={classes.avatar} src={imageavatar}/>
 
             </div>
         </nav>
