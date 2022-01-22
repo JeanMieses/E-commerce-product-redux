ddd568feff4c3883b6af90b232d4baae9ee892fd
JeanMieses <araujo0275@gmail.com>
2022-01-22 00:14:40 -0500

    Styling app

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -12,33 +12,33 @@ const Product = () => {
         setCounter(previous => previous - 1)
     }
 
-    return (<div className={classes.product}>
-        <h2>Fall Limited Edition Sneakers </h2>
-        <p>
-            These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they’ll withstand everything the w
-            eather can offer.
-        </p>
+    return (
+        <div className={classes.product}>
+            <h2>Fall Limited Edition Sneakers </h2>
+            <p>
+                These low-profile sneakers are your perfect casual wear companion.
+                Featuring a durable rubber outer sole, they’ll withstand everything the w
+                eather can offer.
+            </p>
 
-        <div className={classes.price}>
-            <div>
-                <p>$125.00</p>
-                <p>50%</p>
+            <div className={classes.prices}>
+                <div>
+                    <p className={classes.price}>$125.00</p>
+                    <p className={classes.discount} >50%</p>
+                </div>
+                <p className={classes.originalPrice}>$250.00</p>
             </div>
 
-            <p>$250.00</p>
-        </div>
 
+            <div className={classes.control}>
+                <button onClick={decrease}>-</button>
+                <div> {counter} </div>
+                <button onClick={increase} >+</button>
+            </div>
 
-        <div>
-            <button onClick={decrease}>-</button>
-            <div> {counter} </div>
-            <button onClick={increase} >+</button>
-        </div>
+            <button>add to cart</button>
 
-        <button>add to cart</button>
-
-    </div>)
+        </div>)
 }
 
 
--- src/components/corousel/MainCarousel.js
+++ src/components/corousel/MainCarousel.js
@@ -1,23 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Carousel.module.css';
 import Carousel from './Carousel';
 
 const MainCarousel = () => {
-    const [showThumbnail, setShowThumbnail] = useState(true);
     const main = useRef();
     const thumbnail = useRef();
-    const mainCorouselOptions = { height: '620px', autoplay: true, arrow: 'slider', rewind: true, width: 700, gap: '1rem', breakpoints: {425: {height: '330px',  width: '100%'}}};
-    const thumbnailOptions = { arrows: false, fixedWidth: 80, fixedHeight: 100, gap: 30, rewind: true, pagination: false, cover: true, isNavigation: true };
-    
-     window.addEventListener('resize', () => {
-        if(window.innerWidth < 1024) {
-            setShowThumbnail(false);
-
-        } else if (window.innerWidth > 1024) {
-            setShowThumbnail(true);
-            main.current.sync(thumbnail.current.splide);
-        }
-     })
+    const mainCorouselOptions = { height: '620px', autoplay: true, arrow: 'slider', rewind: true, width: 700, gap: '1rem', breakpoints: { 425: { height: '330px', width: '100%' } } };
+    const thumbnailOptions = { rewind: false, arrows: false, fixedWidth: 90, fixedHeight: 90, gap: 30, rewind: true, pagination: false, cover: true, isNavigation: true };
 
     useEffect(() => {
         main.current.sync(thumbnail.current.splide);
@@ -25,8 +14,9 @@ const MainCarousel = () => {
 
     return (<div className={classes.carousel}>
         <Carousel ref={(slider) => (main.current = slider)} configurations={mainCorouselOptions} />
-        <br/>
-        {showThumbnail && <Carousel ref={(slider) => (thumbnail.current = slider)} configurations={thumbnailOptions} />}
+        <div className={classes.hide}>
+            <Carousel ref={(slider) => (thumbnail.current = slider)} configurations={thumbnailOptions} />
+        </div>
     </div>
     )
 }
