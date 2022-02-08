439703e5edd78c35928b9a8c9cc2fed1bd7618a2
JeanMieses <araujo0275@gmail.com>
2022-02-08 10:46:12 -0500

    adding success animation

--- src/components/Product/AddedToCart.js
+++ src/components/Product/AddedToCart.js
@@ -1,12 +1,23 @@
+import Lottie from 'react-lottie';
 import classes from './AddedToCart.module.css';
+import success from './../../assets/success.json';
+
+const defaultOptions = {
+    loop: false,
+    autoplay: true, 
+    animationData: success,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
 const AddedToCart = (props) => {
     return (<div className={classes.AddedToCart}>
         <div>
-            <p>Adding</p>
-            <strong><p>{props.product}</p></strong>
-            <p>to your cart</p>
-            {/* <p>Total Price: ${props.totalPrice}</p> */}
+            <Lottie options={defaultOptions}  height={50} width={50}/>
+            <p><strong>{props.product}</strong> </p>
+           <p>added to the cart</p>
+    
         </div>
     </div>)
 }
